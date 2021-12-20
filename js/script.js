b7d65cb4a55b50dde0421b5e7a4207373eded475
DavideVin01 <davidevinciguerradv01@gmail.com>
2021-12-20 20:25:24 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log('JS OK');
+
+
+/* TRACCIA:
+
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+
+/*
+0 - Recuperare un elemento in pagina e preparare messaggio contenente Password
+
+1 - Chiedere il nome all'utente
+2 - Chiedere il cognome all'utente
+3 - Chiedere il colore preferito all'utente
+4 - Stampare nomeCognomeColorePreferito21
+*/
